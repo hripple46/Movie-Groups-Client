c8a7e87be2043e41c01f44b94f5f314309ca503d
Henry Ripple <hripple46@gmail.com>
2023-06-19 13:53:53 -0500

    configured initial api structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      fetch("http://localhost:3000/api/test")
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .then((data) => console.log(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My first API</h1>
+      <p>{data.message}</p>
     </div>
   );
 }
