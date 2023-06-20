c730675aaea6def28d99f084eac54765fd2b9e36
Henry Ripple <hripple46@gmail.com>
2023-06-20 12:48:41 -0500

    messages are added to DB with POST request

--- src/App.js
+++ src/App.js
@@ -5,23 +5,42 @@ import Message from "./Message.js";
 
 function App() {
   const [data, setData] = useState({});
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
-    fetchData();
+    fetchMessages();
   }, []);
 
-  const fetchData = async () => {
+  const fetchMessages = async () => {
     try {
-      fetch("http://localhost:3000/api/test")
-        .then((response) => response.json())
-        .then((data) => setData(data))
-        .then((data) => console.log(data));
+      fetch("http://localhost:3000/api/message", { type: "GET" })
+        .then((res) => res.json())
+        .then((data) => {
+          setMessages(data);
+        });
     } catch (err) {
       console.log(err);
     }
   };
 
+  const renderMessages = () => {
+    console.log(messages);
+    if (!messages) {
+      return <p>No messages found</p>;
+    } else {
+      return (
+        <ul>
+          {messages.map((message) => (
+            <li key={message._id}>{message.text}</li>
+          ))}
+        </ul>
+      );
+    }
+  };
+
   return (
     <div className="App">
+      <h1>Messages</h1>
+      {renderMessages()}
       <Message />
     </div>
   );
--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 export default function Message() {
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const currentText = text;
+    setText("");
+    const response = await fetch("http://localhost:3000/api/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: currentText }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="text">Message:</label>
-        <input type="text" id="text" name="text" />
+        <input
+          type="text"
+          id="text"
+          name="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
     </div>
