0d352e1d481e171703a7c754114b3ef01d18205f
Henry Ripple <hripple46@gmail.com>
2023-06-25 18:23:12 -0500

    User can type in group name to be added to pending list

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function DashBoard() {
   const [user, setUser] = useState("Not Signed In");
   const [group, setGroup] = useState({});
+  const [joinGroupName, setJoinGroupName] = useState();
 
   useEffect(() => {
     console.log(document.cookie);
@@ -58,6 +59,27 @@ export default function DashBoard() {
     });
   };
 
+  const joinGroup = async (e) => {
+    e.preventDefault();
+    let checkForCookie = getCookie("token");
+    if (!checkForCookie) {
+      alert("You are not signed in.");
+      return;
+    }
+    let cookieValue = JSON.parse(getCookie("token"));
+    let token = cookieValue.token;
+    fetch("http://localhost:3000/api/groups/join", {
+      method: "POST",
+      body: JSON.stringify(joinGroupName),
+      headers: {
+        "Content-type": "application/json",
+        authorization: "bearer " + token,
+      },
+    }).then((response) => {
+      console.log(response.json());
+    });
+  };
+
   return (
     <div>
       Hello, {user}
@@ -69,6 +91,15 @@ export default function DashBoard() {
           onChange={(e) => setGroup({ groupName: e.target.value })}
         ></input>
         <button type="submit" onClick={submitNewGroup}></button>
+      </form>
+      <form>
+        <label htmlFor="joinGroup">Group Name: </label>
+        <input
+          type="text"
+          name="joinGroup"
+          onChange={(e) => setJoinGroupName({ joinGroupName: e.target.value })}
+        ></input>
+        <button type="submit" onClick={joinGroup}></button>
       </form>
     </div>
   );
